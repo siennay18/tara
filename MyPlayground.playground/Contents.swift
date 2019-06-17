5fd36412987a1a1343602e497819c63f52446e1f
Apple <apple@Apples-MacBook-Pro-8.local>
2019-06-17 11:06:21 -0700

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+//var name = "Sophia"
+//print("Hello, \(name)")
+
+var first = "Karlie"
+var last = "Kloss"
+var love = "love"
+var I = "I"
+print("\(first)\(last)")
+print("\(last)\(first)")
+print("\(first) \(last)")
+print("\(last) \(first) \(last) \(first)")
+print("\(I) \(love) \(first) \(last)")
+10/3
+10.0/3.0
+
+var bigNumber = 15
+var smallNumber = 2.0
+
+Double(bigNumber)/smallNumber
+
+5<3
+12>7
+6 != 8
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+//var favoriteFood = "Chipotle"
+
+//if favoriteFood == "Chipotle" {
+   // print("I love Love LOVE salads")
+//}
+
+//else if favoriteFood == "starbucks" {
+   // print("nope")
+//}
+
+//else {
+    //print("well, idk what to say now")
+//}
+
+var hasAccount = false
+
+if hasAccount == false {
+    print("Please log in!")
+}
+
+else {
+    print("Let's get some information to create an account to you.")
+}
+
+
+var grade = 9
+
+if grade <= 8 {
+    print("You are in Middle School.")
+}
+
+else if grade <= 5 {
+    print("You are in Elementary School")
+}
+
+else if grade >= 9 {
+    print("You are in High School")
+}
+
+else {
+    print("You are in college. ")
+}
